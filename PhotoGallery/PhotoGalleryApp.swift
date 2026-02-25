fd13e83bf588945830db5ef3d5b39fd94ae7e863
aryanarayantiwari <aryanarayantiwari@gmail.com>
2026-02-26 00:11:19 +0530

    Initial Commit

---
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Arya Narayan Tiwari on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
